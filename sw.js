3e937c105d74275eeb65bb71115cbe75bc54f17d
MODULAJW <munirdandesi@gmail.com>
2026-06-06 22:46:26 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+var CACHE_NAME = 'vgatech-player-v1';
+var urlsToCache = [
+  '/VGA-TECH/VGATECH.html',
+  '/VGA-TECH/file2_style.css',
+  '/VGA-TECH/file2_script.js',
+  '/VGA-TECH/logo/logo.jpg',
+  'https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,400;14..32,500;14..32,600;14..32,700;14..32,800&display=swap',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
